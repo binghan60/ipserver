376b6d511180cab8732eeae52e8e1ffd083ee0f2
binghan60 <binghan60@gmail.com>
2025-06-24 17:31:09 +0800

    更新

--- index.js
+++ index.js
@@ -14,16 +14,24 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(requestIp.mw());
-// request-ip 套件版
-app.get('/requestIP', (req, res) => {
-  const ip = req.clientIp;
-  res.send(`你的 IP 是：${ip}`);
+
+app.set('trust proxy', true); // 有 proxy 一定要開
+
+app.get('/ip', (req, res) => {
+  const requestIpClient = req.clientIp; // request-ip 套件抓的 IP
+  let expressIp = req.ip; // Express 內建的 req.ip
+
+  // 把 ::ffff: 開頭的 IPv6 映射格式轉成 IPv4
+  if (expressIp && expressIp.startsWith('::ffff:')) {
+    expressIp = expressIp.substring(7);
+  }
+
+  res.json({
+    requestIpClient,
+    expressIp,
+  });
 });
-// 純 EXPRESS 框架版
-app.get('/expressIP', (req, res) => {
-  const ip = req.ip;
-  res.send(`你的 IP 是：${ip}`);
-});
+
 app.listen(PORT, () => {
   console.log(`BFF Server運行於 http://localhost:${PORT}`);
 });
